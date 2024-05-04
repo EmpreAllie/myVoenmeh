aa5ff2c01ed7b1ddb41ed3529e544fdfed8653f7
edya <o721b19@voenmeh.ru>
2024-05-04 15:13:21 +0300

    Почта вернулась на место

--- app/src/main/java/com/my/voenmeh/Activities/MailActivity.java
+++ app/src/main/java/com/my/voenmeh/Activities/MailActivity.java
@@ -3,6 +3,8 @@ package com.my.voenmeh.Activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -16,11 +18,28 @@ import com.my.voenmeh.R;
 
 public class MailActivity extends AppCompatActivity {
 
+    private WebView webView; // Объявляем WebView
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_mail);
+
+        // Находим WebView по id
+        webView = findViewById(R.id.mailView);
+        webView.getSettings().setJavaScriptEnabled(true); // Включаем поддержку JavaScript
+        webView.loadUrl("https://mail.voenmeh.ru/mail"); // Загружаем страницу почты
+
+        // Настройки WebView для открытия ссылок внутри приложения, а не в браузере
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mail), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -39,22 +58,18 @@ public class MailActivity extends AppCompatActivity {
                     startActivity(new Intent(getApplicationContext(), NewsActivity.class));
                     overridePendingTransition(0, 0);
                     return true;
-                }
-                else if (id == R.id.navigation_tracker) {
+                } else if (id == R.id.navigation_tracker) {
                     startActivity(new Intent(getApplicationContext(), TrackerActivity.class));
                     overridePendingTransition(0, 0);
                     return true;
-                }
-                else if (id == R.id.navigation_schedule) {
+                } else if (id == R.id.navigation_schedule) {
                     startActivity(new Intent(getApplicationContext(), ScheduleActivity.class));
                     overridePendingTransition(0, 0);
                     return true;
-                }
-                else if (id == R.id.navigation_mail) {
-
+                } else if (id == R.id.navigation_mail) {
+                    // Уже находимся во вкладке "Mail", ничего не делаем
                     return true;
-                }
-                else if (id == R.id.navigation_service) {
+                } else if (id == R.id.navigation_service) {
                     startActivity(new Intent(getApplicationContext(), ServicesActivity.class));
                     overridePendingTransition(0, 0);
                     return true;
