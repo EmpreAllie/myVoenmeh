19f03ec0b68c883a9d2e0df6ea57e582be8317b1
edya <o721b19@voenmeh.ru>
2024-05-12 21:34:16 +0300

    Всё стало ещё лучше

--- app/src/main/java/com/my/voenmeh/ui/news/NewsPost.java
+++ app/src/main/java/com/my/voenmeh/ui/news/NewsPost.java
@@ -4,34 +4,44 @@ import static android.view.ViewGroup.LayoutParams;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.my.voenmeh.Utils.Constants;
-import com.my.voenmeh.R;
-import com.squareup.picasso.Picasso;
+import androidx.core.content.res.ResourcesCompat;
 
-import org.w3c.dom.ls.LSOutput;
+import com.my.voenmeh.R;
+import com.my.voenmeh.Utils.Constants;
+import com.squareup.picasso.Picasso;
 
 public class NewsPost {
     TextView postText = null;
     ImageView postImage = null;
+
     public NewsPost(Context mContext) {
-        NewsRepository nr = new NewsRepository(); // здесь заполняется список из медиа для постов
+        NewsRepository nr
+                = new NewsRepository(); // здесь заполняется список из медиа для постов
 
         // фигня для доступа к вьюшкам в активити
         Activity a = (Activity) mContext;
         LinearLayout ll = (LinearLayout) a.findViewById(R.id.news_posts);
 
-        //int count = a.getResources().getInteger(R.integer.NUMBER_OF_POSTS);
+        // int count = a.getResources().getInteger(R.integer.NUMBER_OF_POSTS);
 
         for (int index = 0; index < Constants.NUMBER_OF_POSTS; index++) {
 
-            // создание одного текствью
             postText = new TextView(mContext);
             postText.setText(nr.listOfPosts.get(index).getText());
-            postText.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+            postText.setLayoutParams(new LinearLayout.LayoutParams(
+                    LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+
+            Typeface typeface = ResourcesCompat.getFont(mContext, R.font.montserrat_m);
+            postText.setTypeface(typeface);
+
+            postText.setTextSize(15);
+            postText.setTextColor(Color.BLACK);
             ll.addView(postText);
 
             // создание одной картинки
@@ -40,9 +50,9 @@ public class NewsPost {
             System.out.println("url: " + url);
             if (url != "") {
                 Picasso.get().load(url).into(postImage);
-            }
-            else {
-                String unavailablePath = "https://eagle-sensors.com/wp-content/uploads/unavailable-image.jpg";
+            } else {
+                String unavailablePath
+                        = "https://eagle-sensors.com/wp-content/uploads/unavailable-image.jpg";
                 Picasso.get().load(unavailablePath).into(postImage);
             }
             ll.addView(postImage);
